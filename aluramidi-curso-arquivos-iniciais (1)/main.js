a6907e5a148e574ecf4728f0456b032e9d8975eb
VinneGaho <98663243+ViniciusGarro@users.noreply.github.com>
2022-10-15 15:34:15 -0300

    projeto mid

--- aluramidi-curso-arquivos-iniciais (1)/main.js
+++ aluramidi-curso-arquivos-iniciais (1)/main.js
@@ -1,6 +1,37 @@
-function tocaSomPom () {
-document.querySelector('#som_tecla_pom').play();
 
+function tocaSom (seletorAudio) {
+
+ const elemento = document.querySelector(seletorAudio);
+
+ if (elemento != null && elemento.localName === 'audio') {
+    
+    elemento.play();
+    }
+    else {
+        alert('Elemento não encontrado ou seletor inválido');
+    }
+ }
+
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+     const instrumeto = tecla.classList[1];
+     const idAudio = `#som_${instrumeto}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (evento) {
+        console.log(evento);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+        tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
-document.querySelector('.tecla_pom');
-
